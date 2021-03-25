0dbc381bdf9cd8eb848347fd7de0a12f5c61b7e6
Stephen Jackson <jjackson19862017@gmail.com>
2021-03-25 12:10:55 +0000

    Send confirmation email to subscriber

--- app/Http/Livewire/LandingPage.php
+++ app/Http/Livewire/LandingPage.php
@@ -3,6 +3,8 @@
 namespace App\Http\Livewire;
 
 use App\Models\Subscriber;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 use Illuminate\Support\Facades\Log;
 
@@ -17,9 +19,18 @@ class LandingPage extends Component
     public function subscribe()
     {
         $this->validate();
-        $subscriber = Subscriber::create([
-            'email' => $this->email,
-        ]);
+
+        DB::transaction(function () {
+            $subscriber = Subscriber::create([
+                'email' => $this->email,
+            ]);
+
+            $notification = new VerifyEmail; // Creates new VerifyEmail instance
+
+            $subscriber->notify($notification); // Sends comfirmation from the new instance
+
+        }, $deadlockRestries = 5);
+
         $this->reset('email');
     }
 
