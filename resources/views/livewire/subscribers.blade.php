c6f8cce7916d69eecae9618e5bb4c4a78fde79c6
Stephen Jackson <jjackson19862017@gmail.com>
2021-03-25 16:02:21 +0000

    Allowing to Remove a Subscriber Using Livewire

---
+++ resources/views/livewire/subscribers.blade.php
@@ -0,0 +1,45 @@
+<div class="p-6 bg-white border-b border-gray-200">
+    <p class="mb-6 text-2xl font-bold text-gray-600 underline">
+        Subscribers
+    </p>
+    @if ($subscribers->isEmpty())
+    <div class="flex w-full p-5 bg-red-100 rounded-lg">
+        <p class="text-red-600">
+            No Subscribers found!
+        </p>
+    </div>
+    @else
+    <table class="w-full table-fixed">
+        <thead class="text-indigo-600 border-b-2 border-gray-300">
+            <tr class="text-sm text-indigo-900 border-b border-gray-400">
+                <th class="w-1/2 px-6 py-3 text-left">Email</th>
+                <th class="w-1/4 px-6 py-3 text-left">Verified</th>
+                <th class="w-1/4"></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($subscribers as $subscriber)
+            <tr class="text-sm text-indigo-900 border-b border-gray-400">
+                <td class="px-6 py-4">
+                    {{$subscriber->email}}
+                </td>
+                <td class="px-6 py-4">
+                    {{optional($subscriber->email_verified_at)->diffForHumans() ?? 'Never'}}
+                </td>
+                <td class="px-6 py-4">
+                    <x-button class="text-red-500
+                    border
+                    border-red-500
+                    bg-red-50
+                    hover:bg-red-200" wire:click='delete({{$subscriber->id}})'>
+                        Delete
+                    </x-button>
+                </td>
+            </tr>
+            @endforeach
+
+        </tbody>
+
+    </table>
+    @endif
+</div>
--- resources/views/subscribers/all.blade.php
+++ resources/views/subscribers/all.blade.php
@@ -8,41 +8,7 @@
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <p class="mb-6 text-2xl font-bold text-gray-600 underline">
-                        Subscribers
-                    </p>
-                    @if ($subscribers->isEmpty())
-                    <div class="flex w-full p-5 bg-red-100 rounded-lg">
-                        <p class="text-red-600">
-                            No Subscribers found!
-                        </p>
-                    </div>
-                    @else
-                    <table class="w-full table-fixed">
-                        <thead class="text-indigo-600 border-b-2 border-gray-300">
-                            <tr class="text-sm text-indigo-900 border-b border-gray-400">
-                                <th class="w-3/4 px-6 py-3 text-left">Email</th>
-                                <th class="w-1/4 px-6 py-3 text-left">Verified</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($subscribers as $subscriber)
-                            <tr class="text-sm text-indigo-900 border-b border-gray-400">
-                                <td class="px-6 py-4">
-                                    {{$subscriber->email}}
-                                </td>
-                                <td class="px-6 py-4">
-                                    {{optional($subscriber->email_verified_at)->diffForHumans() ?? 'Never'}}
-                                </td>
-                            </tr>
-                            @endforeach
-
-                        </tbody>
-
-                    </table>
-                    @endif
-                </div>
+                <livewire:subscribers></livewire:subscribers>
             </div>
         </div>
     </div>
