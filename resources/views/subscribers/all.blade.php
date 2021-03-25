711c55c887fe059fd77cca9c77a98921b1380054
Stephen Jackson <jjackson19862017@gmail.com>
2021-03-25 15:44:06 +0000

    Apply Tailwind CSS to Table

--- resources/views/subscribers/all.blade.php
+++ resources/views/subscribers/all.blade.php
@@ -19,7 +19,26 @@
                         </p>
                     </div>
                     @else
-                    <table class="w-full">
+                    <table class="w-full table-fixed">
+                        <thead class="text-indigo-600 border-b-2 border-gray-300">
+                            <tr class="text-sm text-indigo-900 border-b border-gray-400">
+                                <th class="w-3/4 px-6 py-3 text-left">Email</th>
+                                <th class="w-1/4 px-6 py-3 text-left">Verified</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($subscribers as $subscriber)
+                            <tr class="text-sm text-indigo-900 border-b border-gray-400">
+                                <td class="px-6 py-4">
+                                    {{$subscriber->email}}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{optional($subscriber->email_verified_at)->diffForHumans() ?? 'Never'}}
+                                </td>
+                            </tr>
+                            @endforeach
+
+                        </tbody>
 
                     </table>
                     @endif
