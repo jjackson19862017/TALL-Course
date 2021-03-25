d445d85e2d1b4870faf48cf548cefeb7facc093f
Stephen Jackson <jjackson19862017@gmail.com>
2021-03-25 16:15:26 +0000

    Allow Search Field in Subs List

--- app/Http/Livewire/Subscribers.php
+++ app/Http/Livewire/Subscribers.php
@@ -7,6 +7,13 @@ use Livewire\Component;
 
 class Subscribers extends Component
 {
+    public $search = '';
+
+    // Allows browser to remember search string incase of refresh
+    protected $queryString = [
+        'search' => ['except' => '']
+    ];
+
     public function delete(Subscriber $subscriber)
     {
         $subscriber->delete();
@@ -14,7 +21,8 @@ class Subscribers extends Component
 
     public function render()
     {
-        $subscribers = Subscriber::all();
+        $subscribers = Subscriber::where('email', 'like', "%{$this->search}%")->get();
+
         return view('livewire.subscribers')->with([
             'subscribers' => $subscribers,
         ]);
